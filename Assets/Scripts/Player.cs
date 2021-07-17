57ea21ededbd2c08ad145f08808190981d288fdd
Zac <zac.bogner@gmail.com>
2021-07-17 15:54:55 -0700

    Character Controller Optimization and Shooting initialization
    * Optimized player controls
    * imported reticle assets to begin Shooting implementation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI; 
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
     private CharacterController controller;
-    private Vector3 direction;
-    private Vector3 velocity; 
+
+    [Header("Controller Settings")]
     [SerializeField]
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 5.0f;
     [SerializeField]
-    private float jumpHeight = 1.0f;
+    private float jumpHeight = 10.0f;
     [SerializeField]
     private float gravityValue = -9.81f;
+
+    private Vector3 direction;
+    private Vector3 velocity; 
+
+    private Camera mainCam;
+
+    [Header("Camera Settings")]
+    [SerializeField]
+    private float mainCamSensityvity = 1.0f;
 
     public float mouseX;
     public float mouseY;
 
-    private Camera mainCam; 
-
     private void Start()
     {
+        // lock cursor when game starts v
+        Cursor.lockState = CursorLockMode.Locked; 
+
         controller = gameObject.GetComponent<CharacterController>();
         if(controller == null)
         {
@@ -35,9 +47,18 @@ public class Player : MonoBehaviour
     }
 
     void Update()
-    {
-        CharacterMovement(); 
+    {        
+        CharacterMovement();
+        CameraControl(); 
 
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None; 
+        }
+    }
+
+    private void CameraControl()
+    {
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
 
@@ -45,15 +66,14 @@ public class Player : MonoBehaviour
         // Apply mouse Y to Camera x value 
         // transform.eulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + mouseX, transform.localEulerAngles.z); 
         Vector3 currentRotation = transform.localEulerAngles;
-        currentRotation.y += mouseX;
+        currentRotation.y += mouseX * mainCamSensityvity;
         transform.localRotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
 
         // Look up and down
         Vector3 currentCameraRotation = mainCam.gameObject.transform.localEulerAngles;
-        currentCameraRotation.x -= mouseY;
+        currentCameraRotation.x -= mouseY * mainCamSensityvity;
         // mainCam.gameObject.transform.localEulerAngles = currentCameraRotation; 
-        mainCam.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCameraRotation.x, Vector3.right); 
-
+        mainCam.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCameraRotation.x, Vector3.right);
     }
 
     private void CharacterMovement()
@@ -75,6 +95,8 @@ public class Player : MonoBehaviour
         }
 
         velocity.y += gravityValue * Time.deltaTime;
+
+        velocity = transform.TransformDirection(velocity); // velocity turned into local space to world space 
 
         controller.Move(velocity * Time.deltaTime);
     }
